b290c5a0c74b004d92aa626d6f91b8a2592fab9e
Tens1des <romankotov3213@gmail.com>
2025-10-05 13:03:33 +0300

    Initial Commit

---
+++ Cinema Dice/Cinema_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cinema_DiceApp.swift
+//  Cinema Dice
+//
+//  Created by Рома Котов on 05.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct Cinema_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cinema Dice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cinema Dice
+//
+//  Created by Рома Котов on 05.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
